31765b0b015df90782acf1e7e3c0d3a3b53e56b4
Rafael Ponte <rponte@gmail.com>
2012-03-23 03:27:19 -0300

    blank project

--- src/br/com/triadworks/issuetracker/service/Autenticador.java
+++
@@ -1,9 +0,0 @@
-package br.com.triadworks.issuetracker.service;
-
-import br.com.triadworks.issuetracker.model.Usuario;
-
-public interface Autenticador {
-
-	public Usuario autentica(String login, String senha);
-
-}
