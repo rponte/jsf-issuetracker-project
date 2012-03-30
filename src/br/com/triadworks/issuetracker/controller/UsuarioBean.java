b8193886009cc0a5d7529dec7c5793145982ec33
Rafael Ponte <rponte@gmail.com>
2012-03-30 12:21:00 -0300

    4a aula concluida

--- src/br/com/triadworks/issuetracker/controller/UsuarioBean.java
+++ src/br/com/triadworks/issuetracker/controller/UsuarioBean.java
@@ -2,9 +2,11 @@ package br.com.triadworks.issuetracker.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
 
 import br.com.triadworks.issuetracker.controller.util.FacesUtils;
 import br.com.triadworks.issuetracker.dao.UsuarioDao;
@@ -21,15 +23,39 @@ public class UsuarioBean {
 	private UsuarioDao usuarioDao;
 	@ManagedProperty("#{facesUtils}")
 	private FacesUtils facesUtils;
-
+	
 	public String lista() {
 		usuarios = usuarioDao.listaTudo();
 		return null;
 	}
 	
 	public String adiciona() {
+		
+		boolean senhaInvalida = !confirmacaoDeSenha.equals(usuario.getSenha());
+		if (senhaInvalida) {
+			facesUtils.adicionaMensagemDeErro("Senha e confirmação de senha não conferem.");
+			return null;
+		}
+		
 		usuarioDao.salva(usuario);
 		facesUtils.adicionaMensagemDeInformacao("Usuário adicionado com sucesso!");
+		return "lista";
+	}
+	
+	public String remove() {
+		usuarioDao.remove(usuario);
+		facesUtils.adicionaMensagemDeInformacao("Usuário removido com sucesso!");
+		return lista();
+	}
+	
+	public String preparaParaAlterar(Usuario usuario) {
+		this.usuario = usuarioDao.carrega(usuario.getId()); // evita LazyInitializationException
+		return "altera";
+	}
+	
+	public String altera() {
+		usuarioDao.atualiza(usuario);
+		facesUtils.adicionaMensagemDeInformacao("Usuário atualizado com sucesso!");
 		return "lista";
 	}
 	
@@ -53,6 +79,9 @@ public class UsuarioBean {
 	}
 	public void setFacesUtils(FacesUtils facesUtils) {
 		this.facesUtils = facesUtils;
+	}
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
 	}
 }
 
