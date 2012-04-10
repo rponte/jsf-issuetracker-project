963bb1acbaba355a2f7428b200318e301d9387e6
Rafael Ponte <rponte@gmail.com>
2012-04-09 23:42:08 -0300

    Torna entidade serializavel;

--- src/br/com/triadworks/issuetracker/model/Usuario.java
+++ src/br/com/triadworks/issuetracker/model/Usuario.java
@@ -1,5 +1,7 @@
 package br.com.triadworks.issuetracker.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,8 +9,10 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
 @Entity
-public class Usuario {
+public class Usuario implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_USUARIO")
 	@SequenceGenerator(name="SEQ_USUARIO", sequenceName="SEQ_USUARIO", allocationSize=1)
